9dbc4873db0353a776185adc2bf87d8b78ef1900
Yevhenii Pozniak <jekamanu@gmail.com>
2022-05-18 19:55:36 +0300

    update task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+
+function makeMarkup(array) {
+  array.map((element) => {
+    const newItem = document.createElement("li");
+    newItem.textContent = element;
+    newItem.classList.add("item");
+    listEl.append(newItem);
+  });
+}
+
+makeMarkup(ingredients);
